90276c33d2a977f138b8a05e5358e07e33f81b28
lucianabenedito <89881181+lucianabenedito@users.noreply.github.com>
2021-10-05 20:15:33 -0300

    Add files via upload

---
+++ contagem.js
@@ -0,0 +1,12 @@
+var QuantidadeDeAlunos = 14;
+
+for (var Contadorm = 0; contador < QuantidadeDeAlunos; Contador++){
+    if (contdor == 0){
+        console.log("o numero e zero");
+    }
+    else if(contador % 2 == 0){
+        console.log("contador + " o numero e par);
+    }
+else{
+    console.log(contador + "o numero e impar")
+}
